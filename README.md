9448bb56477dc1230d7676b89c314cb3eb397b5e
sahil <leviack629@gmail.com>
2025-01-21 23:41:30 +0530

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Basic of Data Fetching in React
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+<main className="flex justify-center items-center p-20">
+  <h1>Data Fetching</h1>
+</main>
+  );
+}
